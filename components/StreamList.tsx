e25d5b9b80b8065219fec8c8d4131661c0482021
Richard Hofmänner <hello@terillos.dev>
2021-06-01 13:50:43 +0200

    Added Key

--- components/StreamList.tsx
+++ components/StreamList.tsx
@@ -6,7 +6,7 @@ import { Stream as StreamTyping } from '../typings/Stream'
 function StreamList({ streams }: { streams: StreamTyping[] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {streams.map((stream:StreamTyping) => <Stream stream={stream} />)}
+      {streams.map((stream:StreamTyping) => <Stream key={stream.id} stream={stream} />)}
     </div>
   )
 }
